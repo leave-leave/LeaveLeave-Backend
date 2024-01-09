bc161f68076b4229736d5df1369cdce5f4a1cb2c
eeuunn <haspring@dsm.hs.kr>
2024-01-09 10:59:48 +0900

    ⚙️ :: project setting

---
+++ src/main/kotlin/com/example/leavetest/LeaveTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.leavetest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LeaveTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<LeaveTestApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/leavetest/LeaveTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.leavetest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LeaveTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
