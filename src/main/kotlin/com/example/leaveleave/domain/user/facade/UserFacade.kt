fbf6564acc55b1457aa3cf5755b8de50b33d56a8
eeuunn <haspring@dsm.hs.kr>
2024-01-21 15:38:57 +0900

    ADD :: UserFacade

---
+++ src/main/kotlin/com/example/leaveleave/domain/user/facade/UserFacade.kt
@@ -0,0 +1,22 @@
+package com.example.leaveleave.domain.user.facade
+
+import com.example.leaveleave.domain.user.domain.repository.UserRepository
+import com.example.leaveleave.domain.user.exception.UserNotFoundException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+    fun getCurrentUser(): com.example.leaveleave.domain.user.domain.User {
+        val accountId : String = SecurityContextHolder.getContext().authentication.name
+        return getByAccountId(accountId)
+    }
+    fun checkAccountIdExist(accountId: String) : Boolean{
+        return userRepository.existsByAccountId(accountId)
+    }
+    fun getByAccountId(accountId: String): com.example.leaveleave.domain.user.domain.User {
+        return userRepository.findByAccountId(accountId) ?: throw UserNotFoundException
+    }
+}
