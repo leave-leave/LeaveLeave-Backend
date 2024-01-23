b7e3b5cae89cfeaf4a14c019c413aa543c97e236
eeuunn <haspring@dsm.hs.kr>
2024-01-23 18:04:59 +0900

    Chore ::

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
 
     // jpa
     implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+
 }
 
 tasks.withType<KotlinCompile> {
@@ -84,3 +85,4 @@ configurations {
         isCanBeConsumed = false
     }
 }
+
