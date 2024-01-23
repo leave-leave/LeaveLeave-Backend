b4b7f313fd64444e7d7577797c012e5a62ec9376
eeuunn <haspring@dsm.hs.kr>
2024-01-23 18:03:18 +0900

    UPDATE :: checkPassword Request

--- src/main/kotlin/com/example/leaveleave/domain/user/presentation/dto/request/SignUpRequest.kt
+++ src/main/kotlin/com/example/leaveleave/domain/user/presentation/dto/request/SignUpRequest.kt
@@ -25,5 +25,12 @@ data class SignUpRequest(
         message = "소문자, 숫자, 특수문자가 포함되어야 하며 8자~20자 사이여야 합니다."
     )
     val password: String,
+
+    @field:NotBlank
+    @field:Pattern(
+        regexp = "(?=.*[a-z])(?=.*[0-9])(?=.*[!#$%&'()*+,./:;<=>?@＼^_`{|}~])[a-zA-Z0-9!#$%&'()*+,./:;" +
+                "<=>?@＼^_`{|}~]{8,30}$",
+        message = "소문자, 숫자, 특수문자가 포함되어야 하며 8자~20자 사이여야 합니다."
+    )
     val checkPassword: String,
 )
