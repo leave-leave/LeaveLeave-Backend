3c942a43928973c9503b3475f0ddd68774d2fbd0
eeuunn <haspring@dsm.hs.kr>
2024-01-25 11:32:57 +0900

    UPDATE :: String? 으로 변경

--- src/main/kotlin/com/example/leaveleave/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/example/leaveleave/global/security/auth/AuthDetailsService.kt
@@ -4,13 +4,14 @@ import com.example.leaveleave.domain.user.facade.UserFacade
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
+import javax.persistence.Id
 
 @Service
 class AuthDetailsService(
     private val userFacade: UserFacade
 ) : UserDetailsService {
-    override fun loadUserByUsername(username: String?): UserDetails {
-        val user = userFacade.getByAccountId(accountId = "")
+    override fun loadUserByUsername(accountId: String): UserDetails {
+        val user = userFacade.getByAccountId(accountId)
         return AuthDetails(user)
     }
 }
