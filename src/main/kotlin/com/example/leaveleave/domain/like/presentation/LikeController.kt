3de6d32cf46d405f9d13cd84291f5f8784c463ca
eeuunn <haspring@dsm.hs.kr>
2024-01-30 23:48:24 +0900

    UPDATE :: LikeController

--- src/main/kotlin/com/example/leaveleave/domain/like/presentation/LikeController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/presentation/LikeController.kt
@@ -3,12 +3,7 @@ package com.example.leaveleave.domain.like.presentation
 import com.example.leaveleave.domain.like.service.LikeService
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 
@@ -18,16 +13,15 @@ class LikeController(
     private val likeService: LikeService,
 ) {
 
-    @ResponseStatus(HttpStatus.OK)
-    @PostMapping("/create")
-    fun createLike(authentication: Authentication, @RequestParam feedId: Long){
-        likeService.createLike(authentication.toString(), feedId)
-        return
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/{feed-id}")
+    fun createLike(@PathVariable("feed-id") feedId: Long){
+        likeService.createLike(feedId)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @PostMapping("/cancel")
-    fun cancelLike(authentication: Authentication, @RequestParam feedId: Long){
-        likeService.cancelLike(authentication.toString(), feedId)
+    @DeleteMapping("/{feed-id}")
+    fun cancelLike(@PathVariable("feed-id") feedId: Long, authentication: Authentication){
+        likeService.cancelLike(feedId)
     }
 }
