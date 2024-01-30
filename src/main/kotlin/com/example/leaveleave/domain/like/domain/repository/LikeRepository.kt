cab20ac0ddc14f6f7367d75cb8e72fd2cdd0cbbc
eeuunn <haspring@dsm.hs.kr>
2024-01-30 21:15:06 +0900

    UPDATE :: LIKE Repository/Service

--- src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
@@ -3,6 +3,7 @@ package com.example.leaveleave.domain.like.domain.repository
 import com.example.leaveleave.domain.like.domain.Like
 import com.example.leaveleave.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.function.LongFunction
 
 interface LikeRepository : JpaRepository <Like, Long> {
     fun findByFeedIdAndUserId(feedId : Long, userId : String): Like?
--- src/main/kotlin/com/example/leaveleave/domain/like/service/LikeService.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/service/LikeService.kt
@@ -5,13 +5,9 @@ import com.example.leaveleave.domain.feed.exception.FeedNotFoundException
 import com.example.leaveleave.domain.like.domain.Like
 import com.example.leaveleave.domain.like.domain.repository.LikeRepository
 import com.example.leaveleave.domain.like.exception.AlreadyExistsLikeException
-import com.example.leaveleave.domain.user.domain.User
 import com.example.leaveleave.domain.user.domain.repository.UserRepository
 import com.example.leaveleave.domain.user.exception.UserNotFoundException
-import com.example.leaveleave.domain.user.facade.UserFacade
-import com.example.leaveleave.global.security.SecurityConfig
-import org.apache.catalina.security.SecurityUtil
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties.Authentication
 import org.springframework.stereotype.Service
 
 @Service
@@ -20,11 +16,11 @@ class LikeService(
     private val userRepository: UserRepository,
     private val feedRepository: FeedRepository,
 ) {
-    fun createLike(feedId: Long, userId: String){
+    fun createLike(userId: String,feedId: Long){
         val user = userRepository.findById(userId).orElseThrow {UserNotFoundException}
         val feed = feedRepository.findById(feedId).orElseThrow {FeedNotFoundException}
 
-        val existingLike = likeRepository.findByFeedIdAndUserId(feedId, userId)
+        val existingLike = likeRepository.findByFeedIdAndUserId(feedId,userId)
         if(existingLike == null){
             likeRepository.save(Like(userId = userId, feedId = feedId))
         } else {
