cb5ee29e7cbb299dedb58ee4d05a6f37b27e21ef
eeuunn <haspring@dsm.hs.kr>
2024-01-30 22:09:12 +0900

    ADD :: LikeController

--- src/main/kotlin/com/example/leaveleave/domain/like/presentation/LikeController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/presentation/LikeController.kt
@@ -4,10 +4,12 @@ import com.example.leaveleave.domain.like.service.LikeService
 import org.apache.tomcat.util.net.openssl.ciphers.Authentication
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 
 @RestController
@@ -19,7 +21,13 @@ class LikeController(
     @ResponseStatus(HttpStatus.OK)
     @PostMapping("/create")
     fun createLike(authentication: Authentication, @RequestParam feedId: Long){
-        likeService.createLike(authentication ,feedId)
+        likeService.createLike(authentication.toString(), feedId)
+        return
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/cancel")
+    fun cancelLike(authentication: Authentication, @RequestParam feedId: Long){
+        likeService.cancelLike(authentication.toString(), feedId)
+    }
 }
