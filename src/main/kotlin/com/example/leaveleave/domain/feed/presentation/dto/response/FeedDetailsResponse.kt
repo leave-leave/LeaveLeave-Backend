32509625aa9625f1069e729d125a3a45ee556482
eeuunn <haspring@dsm.hs.kr>
2024-01-30 22:11:27 +0900

    UPDATE :: UUID 삭제

--- src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/FeedDetailsResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/FeedDetailsResponse.kt
@@ -1,9 +1,8 @@
 package com.example.leaveleave.domain.feed.presentation.dto.response
 
-import java.util.UUID
 
 data class FeedDetailsResponse (
-    val id: UUID,
+    val id: Long,
     val title: String,
     val content: String,
     val username: String,
--- src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/FeedElement.kt
+++ src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/FeedElement.kt
@@ -1,9 +1,8 @@
 package com.example.leaveleave.domain.feed.presentation.dto.response
 
-import java.util.UUID
 
 data class FeedElement (
-    val id: UUID,
+    val id: Long?,
     val title: String,
     val content: String,
 )
