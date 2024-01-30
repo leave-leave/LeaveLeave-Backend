cd429d41cbca61d89bfe5bcd1c15a0c62bf47756
eeuunn <haspring@dsm.hs.kr>
2024-01-30 22:12:02 +0900

    UPDATE :: FeedFacade

--- src/main/kotlin/com/example/leaveleave/domain/feed/facade/FeedFacade.kt
+++ src/main/kotlin/com/example/leaveleave/domain/feed/facade/FeedFacade.kt
@@ -3,6 +3,8 @@ package com.example.leaveleave.domain.feed.facade
 import com.example.leaveleave.domain.feed.domain.Feed
 import com.example.leaveleave.domain.feed.domain.repository.FeedRepository
 import com.example.leaveleave.domain.feed.exception.FeedNotFoundException
+import com.example.leaveleave.domain.feed.presentation.dto.response.FeedElement
+import com.example.leaveleave.domain.user.domain.User
 import org.springframework.stereotype.Component
 
 @Component
@@ -11,5 +13,11 @@ class FeedFacade(
 ) {
     fun getByFeedId(feedID: Long): Feed{
         return feedRepository.findById(feedID).orElseThrow{FeedNotFoundException}
+    }
+    fun getFeedList(feeds: List<Feed>, user: User): List<FeedElement>{
+        return feeds.map{feed ->
+            FeedElement(feed.feedId, feed.title, feed.content)
+
+        }
     }
 }
