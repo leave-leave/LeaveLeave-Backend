6bd8889e43fa63f186368aa18d6e77839e1517e1
eeuunn <haspring@dsm.hs.kr>
2024-01-31 23:08:12 +0900

    ADD :: Question Entity 추가

--- src/main/kotlin/com/example/leaveleave/domain/question/domain/Question.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/domain/Question.kt
@@ -1,4 +1,18 @@
 package com.example.leaveleave.domain.question.domain
 
-class Question {
-}
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+
+@Entity(name = "tbl_question")
+data class Question (
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+
+    @Column(columnDefinition = "varchar(255)")
+    val question: String? = null
+)
