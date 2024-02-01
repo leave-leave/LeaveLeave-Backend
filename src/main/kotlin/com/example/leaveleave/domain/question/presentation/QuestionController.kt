c848c6c3e8914853e684267d4557b728d4a30ccb
eeuunn <haspring@dsm.hs.kr>
2024-02-01 10:34:58 +0900

    ADD :: QuestionController

--- src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
@@ -1,4 +1,17 @@
 package com.example.leaveleave.domain.question.presentation
 
-class QuestionController {
+import com.example.leaveleave.domain.question.presentation.dto.response.QuestionResponse
+import com.example.leaveleave.domain.question.service.QuestionService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class QuestionController(
+    private val questionService: QuestionService
+) {
+    @GetMapping("/question")
+    fun getQuestion(questionId: Long): QuestionResponse{
+        return questionService.getQuestion(questionId)
+    }
+
 }
--- src/main/kotlin/com/example/leaveleave/domain/question/service/QuestionController.kt
+++
@@ -1,4 +0,0 @@
-package com.example.leaveleave.domain.question.service
-
-class QuestionController {
-}
