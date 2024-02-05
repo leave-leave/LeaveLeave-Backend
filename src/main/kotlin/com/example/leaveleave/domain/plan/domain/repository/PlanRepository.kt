f9774032a26a9fa04941e0b1bca278343012c930
eeuunn <haspring@dsm.hs.kr>
2024-02-05 12:55:56 +0900

    ADD :: PlanRepository

---
+++ src/main/kotlin/com/example/leaveleave/domain/plan/domain/repository/PlanRepository.kt
@@ -0,0 +1,7 @@
+package com.example.leaveleave.domain.plan.domain.repository
+
+import com.example.leaveleave.domain.plan.domain.Plan
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface PlanRepository: JpaRepository<Plan,Long> {
+}
