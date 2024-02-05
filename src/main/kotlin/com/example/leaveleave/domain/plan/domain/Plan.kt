7c444efada0badbcfa3d6bad0a92fc1436b1c8ea
eeuunn <haspring@dsm.hs.kr>
2024-02-05 12:56:06 +0900

    UPDATE :: Plan entity

--- src/main/kotlin/com/example/leaveleave/domain/plan/domain/Plan.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/domain/Plan.kt
@@ -1,6 +1,7 @@
 package com.example.leaveleave.domain.plan.domain
 
 import com.example.leaveleave.domain.user.domain.User
+import java.time.LocalDateTime
 import javax.persistence.*
 
 
@@ -12,11 +13,24 @@ class Plan (
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "plan_id")
-    var user: User? = null
+    var user: User? = null,
 
-    
+    @Column(name = "start_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    var startDate: LocalDateTime? = null,
+
+    @Column(name = "end_date")
+    @Temporal(TemporalType.TIMESTAMP)
+    var endDate: LocalDateTime? = null,
+
+    @Column(name = "title")
+    var title: String? = null
+
 ){
-    fun save(user: User){
+    fun save(user: User, startDate: LocalDateTime, endDate: LocalDateTime){
         this.user = user
+        this.startDate = startDate
+        this.endDate = endDate
+        this.title = title
     }
 }
