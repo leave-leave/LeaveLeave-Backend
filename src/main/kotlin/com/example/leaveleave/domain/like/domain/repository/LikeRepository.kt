a2698cc85abbc820f827c08772fb732ee77b3442
eeuunn <haspring@dsm.hs.kr>
2024-02-09 22:50:28 +0900

    UPDATE :: User list search

--- src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
@@ -3,6 +3,7 @@ package com.example.leaveleave.domain.like.domain.repository
 import com.example.leaveleave.domain.feed.domain.Feed
 import com.example.leaveleave.domain.like.domain.Like
 import com.example.leaveleave.domain.user.domain.User
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.function.LongFunction
@@ -11,4 +12,5 @@ import java.util.function.LongFunction
 interface LikeRepository : JpaRepository <Like, Long> {
     fun getByFeedAndUser(feed : Feed, user : User): Like
     fun existsByFeedAndUser(feed : Feed, user : User): Boolean
+    fun findAllByUser(user: User): List<Like>
 }
