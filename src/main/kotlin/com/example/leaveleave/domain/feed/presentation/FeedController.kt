efbc4e7bc6b7d6576b601d87118365c6340f38a6
eeuunn <haspring@dsm.hs.kr>
2024-02-10 20:12:59 +0900

    ADD :: like getmapping

--- src/main/kotlin/com/example/leaveleave/domain/feed/presentation/FeedController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/feed/presentation/FeedController.kt
@@ -4,11 +4,9 @@ import com.example.leaveleave.domain.feed.domain.Feed
 import com.example.leaveleave.domain.feed.domain.repository.FeedRepository
 import com.example.leaveleave.domain.feed.presentation.dto.request.CreateFeedRequest
 import com.example.leaveleave.domain.feed.presentation.dto.request.UpdateFeedRequest
-import com.example.leaveleave.domain.feed.presentation.dto.response.FeedElement
 import com.example.leaveleave.domain.feed.presentation.dto.response.FeedListResponse
-import com.example.leaveleave.domain.feed.presentation.dto.response.PageFeedListResponse
+import com.example.leaveleave.domain.feed.presentation.dto.response.LikeFeedListResponse
 import com.example.leaveleave.domain.feed.service.*
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
-import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
 
 
@@ -31,37 +28,50 @@ class FeedController(
     private val updateFeedService: UpdateFeedService,
     private val getFeedService: GetFeedService,
     private val feedRepository: FeedRepository,
-    private val getListService: GetListService
+    private val getLikeFeedListService: GetLikeFeedListService,
+    private val getFeedListService: GetFeedListService
 ) {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping
-    fun createFeed(@RequestBody @Valid request: CreateFeedRequest){
+    fun createFeed(@RequestBody @Valid request: CreateFeedRequest) {
         createFeedService.execute(request)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PatchMapping("/{feed-id}")
-    fun updateFeed(@PathVariable("feed-id") feedId: Long, @RequestBody @Valid request: UpdateFeedRequest){
-        updateFeedService.execute(feedId,request)
+    fun updateFeed(@PathVariable("feed-id") feedId: Long, @RequestBody @Valid request: UpdateFeedRequest) {
+        updateFeedService.execute(feedId, request)
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{feed-id}")
-    fun deleteFeed(@PathVariable("feed-id") feedId: Long){
+    fun deleteFeed(@PathVariable("feed-id") feedId: Long) {
         feedRepository.deleteById(feedId)
     }
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{feed-id}")
-    fun getFeed(@PathVariable("feed-id") feedId: Long) : ResponseEntity<Feed>{
+    fun getFeed(@PathVariable("feed-id") feedId: Long): ResponseEntity<Feed> {
         val feed = getFeedService.getFeedWithComment(feedId)
         return ResponseEntity.ok(feed)
     }
 
     @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/like")
+    fun getLikeFeedList(): List<LikeFeedListResponse> {
+        val likes = getLikeFeedListService.execute()
+        return likes.map { like ->
+            LikeFeedListResponse(
+                id = like.feed.feedId!!,
+                title = like.feed.title,
+                content = like.feed.content
+            )
+        }
+    }
+
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping()
-    fun getListFeed(){
-        val feed = getListService.execute()
-        return feed
+    fun getFeedList(): List<FeedListResponse>{
+        return getFeedListService.execute()
     }
 }
