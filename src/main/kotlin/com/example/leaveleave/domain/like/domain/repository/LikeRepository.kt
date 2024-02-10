e01666327d6e44d6a989d2ab272adb11b32530c9
eeuunn <haspring@dsm.hs.kr>
2024-02-10 20:13:56 +0900

    CHORE

--- src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/like/domain/repository/LikeRepository.kt
@@ -3,10 +3,10 @@ package com.example.leaveleave.domain.like.domain.repository
 import com.example.leaveleave.domain.feed.domain.Feed
 import com.example.leaveleave.domain.like.domain.Like
 import com.example.leaveleave.domain.user.domain.User
-import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
-import java.util.function.LongFunction
 
 @Repository
 interface LikeRepository : JpaRepository <Like, Long> {
