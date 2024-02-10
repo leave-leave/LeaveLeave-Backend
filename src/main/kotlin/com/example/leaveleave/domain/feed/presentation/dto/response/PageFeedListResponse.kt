f12c30b4fd957365cfa49c11053dd606c1a6d5d7
eeuunn <haspring@dsm.hs.kr>
2024-02-10 20:12:42 +0900

    UPDATE :: name 수정

--- src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/PageFeedListResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/feed/presentation/dto/response/PageFeedListResponse.kt
@@ -3,5 +3,5 @@ package com.example.leaveleave.domain.feed.presentation.dto.response
 data class PageFeedListResponse (
     val totalPage: Int,
     val isFinalPage: Boolean,
-    val feeds: List<FeedElement>
+    val feeds: List<LikeFeedListResponse>
 )
