e325cf6d780e2ed5a862ad3174881fc16b95e8bb
eeuunn <haspring@dsm.hs.kr>
2024-02-12 16:29:31 +0900

    DELETE :: PlanTodoController

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/PlanController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/PlanController.kt
@@ -8,14 +8,7 @@ import com.example.leaveleave.domain.plan.service.PlanService
 import com.example.leaveleave.domain.user.domain.User
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/plans")
@@ -25,7 +18,9 @@ class PlanController(
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
     fun addPlan(@RequestBody planRequest: PlanRequest) {
-        return planService.addPlan(planRequest)
+        planService.addPlan(planRequest)
+        // plan과 todo 배열로 받기
+
     }
 
     @GetMapping("/{plan-id}")
@@ -45,5 +40,11 @@ class PlanController(
     @ResponseStatus(HttpStatus.OK)
     fun deletePlan(@PathVariable("plan-id") planId: Long) {
         planService.deletePlan(planId)
+    }
+
+    @DeleteMapping("/{todo-id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun deleteTodo(@PathVariable("todo-id") todoId: Long) {
+        return planService.deleteTodo(todoId)
     }
 }
