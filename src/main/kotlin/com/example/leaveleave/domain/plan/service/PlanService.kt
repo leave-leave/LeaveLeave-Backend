63390fa532a1c0eaaf90bade7c3d9baf27af5f09
eeuunn <haspring@dsm.hs.kr>
2024-02-12 16:29:44 +0900

    DELETE :: PlanTodoService

--- src/main/kotlin/com/example/leaveleave/domain/plan/service/PlanService.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/service/PlanService.kt
@@ -3,24 +3,37 @@ package com.example.leaveleave.domain.plan.service
 import com.example.leaveleave.domain.plan.domain.Plan
 import com.example.leaveleave.domain.plan.domain.PlanTodoList
 import com.example.leaveleave.domain.plan.domain.repository.PlanRepository
+import com.example.leaveleave.domain.plan.domain.repository.PlanTodoRepository
 import com.example.leaveleave.domain.plan.exception.PlanNotFoundException
 import com.example.leaveleave.domain.plan.presentation.dto.request.PlanRequest
-import com.example.leaveleave.domain.user.domain.User
+import com.example.leaveleave.domain.plan.presentation.dto.request.PlanTodoRequest
 import com.example.leaveleave.domain.user.facade.UserFacade
-import com.example.leaveleave.domain.user.service.UserInfoService
 import org.springframework.dao.EmptyResultDataAccessException
-import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.RequestBody
 import java.time.LocalDateTime
 
 @Service
 class PlanService(
     private val planRepository: PlanRepository,
     private val userFacade: UserFacade,
+    private val planTodoRepository: PlanTodoRepository,
 ) {
-    fun addPlan(@RequestBody request: PlanRequest) {
+    fun addPlan(planRequest: PlanRequest) {
         val user = userFacade.getCurrentUser()
+
+        val newPlan = Plan(
+            id =  planRequest.planId,
+            user = user,
+            title = planRequest.title,
+            startDate = planRequest.startDate,
+            endDate = planRequest.endDate
+        )
+        val savedPlan = planRepository.save(newPlan)
+
+        planRequest.todos.forEach { todo ->
+            val newTodoPlan = PlanTodoList(detailContent = todo.detailContent, plan = savedPlan)
+            planTodoRepository.save(newTodoPlan)
+        }
     }
 
     fun getPlanById(planId: Long): Plan = planRepository.findById(planId).orElseThrow {
@@ -33,6 +46,10 @@ class PlanService(
         } catch (e: EmptyResultDataAccessException) {
             throw PlanNotFoundException("계획을 찾을 수 없음 $planId")
         }
+    }
+
+    fun deleteTodo(todoId: Long) {
+        planTodoRepository.deleteById(todoId)
     }
 }
 
