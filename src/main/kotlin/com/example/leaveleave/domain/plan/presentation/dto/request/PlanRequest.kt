2212cf49010c5d93d9d85fa3d0763b05221a1e04
eeuunn <haspring@dsm.hs.kr>
2024-02-12 16:29:12 +0900

    DELETE :: PlanTodoRequest

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanRequest.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanRequest.kt
@@ -1,11 +1,19 @@
 package com.example.leaveleave.domain.plan.presentation.dto.request
 
+import com.example.leaveleave.domain.plan.presentation.dto.response.PlanTodoResponse
 import com.example.leaveleave.domain.user.domain.User
 import java.time.LocalDateTime
 
 data class PlanRequest(
     val userId: String,
+    val planId: Long,
     val startDate: LocalDateTime,
     val endDate: LocalDateTime,
-    val title: String
+    val title: String,
+    val detailContent: String,
+    val todos: List<PlanTodoRequest>
+)
+
+data class PlanTodoRequest(
+    val detailContent: String
 )
--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanTodoRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.example.leaveleave.domain.plan.presentation.dto.request
-
-import com.example.leaveleave.domain.plan.domain.Plan
-
-data class PlanTodoRequest(
-    val id: Long,
-    val detailContent: String,
-    val plan: Plan
-)
