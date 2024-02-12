8f2e60fe964860c122ac6f109b762907f306c586
eeuunn <haspring@dsm.hs.kr>
2024-02-12 23:07:54 +0900

    DELETE :: id

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanTodoResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanTodoResponse.kt
@@ -4,14 +4,12 @@ import com.example.leaveleave.domain.plan.domain.Plan
 import com.example.leaveleave.domain.plan.domain.PlanTodoList
 
 data class PlanTodoResponse(
-    val id: Long,
     val detailContent: String,
     val plan: Plan
 ) {
     companion object {
         fun fromEntity(planTodoList: PlanTodoList): PlanTodoResponse {
             return PlanTodoResponse(
-                id = planTodoList.id,
                 detailContent = planTodoList.detailContent,
                 plan = planTodoList.plan
             )
