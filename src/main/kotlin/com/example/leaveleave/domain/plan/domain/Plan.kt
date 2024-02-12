5475334093542e50b7bd1ca78d49928b11f6f648
eeuunn <haspring@dsm.hs.kr>
2024-02-12 15:17:45 +0900

    CHORE :: ? = null delete

--- src/main/kotlin/com/example/leaveleave/domain/plan/domain/Plan.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/domain/Plan.kt
@@ -9,7 +9,7 @@ import javax.persistence.*
 class Plan(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Long,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
