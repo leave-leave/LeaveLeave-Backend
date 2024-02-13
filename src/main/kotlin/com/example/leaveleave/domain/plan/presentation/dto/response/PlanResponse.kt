2ba43e885fa87067a7d9d04f82a76d9c97054111
eeuunn <haspring@dsm.hs.kr>
2024-02-13 18:18:28 +0900

    UPDATE :: PlanResponse UPDATE

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanResponse.kt
@@ -1,25 +1,27 @@
 package com.example.leaveleave.domain.plan.presentation.dto.response
 
 import com.example.leaveleave.domain.plan.domain.Plan
+import com.example.leaveleave.domain.plan.domain.PlanTodoList
+import com.example.leaveleave.domain.plan.presentation.dto.request.PlanTodoRequest
 import com.example.leaveleave.domain.user.domain.User
-import com.example.leaveleave.domain.user.presentation.dto.response.UserInfoResponse
-import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 data class PlanResponse(
     val id: Long?,
-    val userId: User,
-    val startDate: LocalDateTime,
-    val endDate: LocalDateTime,
-    val title: String
+    val startDate: ZonedDateTime,
+    val endDate: ZonedDateTime,
+    val title: String,
+    val detailContent : String
+
 ) {
     companion object {
-        fun fromEntity(plan: Plan): PlanResponse {
+        fun fromEntity(plan: Plan, planTodoList: PlanTodoList): PlanResponse {
             return PlanResponse(
                 id = plan.id,
-                userId = plan.user,
                 startDate = plan.startDate,
                 endDate = plan.endDate,
-                title = plan.title
+                title = plan.title,
+                detailContent = planTodoList.detailContent
             )
         }
     }
