cd90002ba55c5ffe56629a4f8707b456cab0f187
eeuunn <haspring@dsm.hs.kr>
2024-02-13 18:18:42 +0900

    UPDATE :: PlanController

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/PlanController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/PlanController.kt
@@ -1,13 +1,10 @@
 package com.example.leaveleave.domain.plan.presentation
 
-import com.example.leaveleave.domain.plan.domain.Plan
-import com.example.leaveleave.domain.plan.exception.PlanNotFoundException
 import com.example.leaveleave.domain.plan.presentation.dto.request.PlanRequest
 import com.example.leaveleave.domain.plan.presentation.dto.response.PlanResponse
+import com.example.leaveleave.domain.plan.presentation.dto.response.PlanTodoListResponse
 import com.example.leaveleave.domain.plan.service.PlanService
-import com.example.leaveleave.domain.user.domain.User
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -20,31 +17,20 @@ class PlanController(
     fun addPlan(@RequestBody planRequest: PlanRequest) {
         planService.addPlan(planRequest)
         // plan과 todo 배열로 받기
-
     }
 
     @GetMapping("/{plan-id}")
     @ResponseStatus(HttpStatus.OK)
-    fun getPlan(@PathVariable("plan-id") planId: Long): ResponseEntity<PlanResponse> {
-        try {
-            val plan = planService.getPlanById(planId)
-        } catch (ex: PlanNotFoundException) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null)
-        }
-
-        return TODO("Provide the return value")
+    fun getPlan(@PathVariable("plan-id") planId: Long): PlanResponse{
+        val plan = planService.getPlanById(planId)
+        val todoList = planService.getTodoPlanId(planId)
+        return PlanResponse.fromEntity(plan, todoList)
     }
-
 
     @DeleteMapping("/{plan-id}")
     @ResponseStatus(HttpStatus.OK)
     fun deletePlan(@PathVariable("plan-id") planId: Long) {
         planService.deletePlan(planId)
-    }
-
-    @DeleteMapping("/{todo-id}")
-    @ResponseStatus(HttpStatus.OK)
-    fun deleteTodo(@PathVariable("todo-id") todoId: Long) {
-        return planService.deleteTodo(todoId)
+        planService.deleteTodo(planId)
     }
 }
