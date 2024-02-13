efa0d5327dc1fcec302f68292b54eb8333135219
eeuunn <haspring@dsm.hs.kr>
2024-02-13 11:12:03 +0900

    ADD :: PlanTodoRequest

---
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanTodoRequest.kt
@@ -0,0 +1,10 @@
+package com.example.leaveleave.domain.plan.presentation.dto.request
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class PlanTodoRequest(
+    @JsonProperty("detailContent")
+    val detailContent: String,
+    val planId : Long,
+    val todoId: Long
+)
