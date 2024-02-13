c06c5e966caa15ea743e3c75f69664a1bae81996
eeuunn <haspring@dsm.hs.kr>
2024-02-13 18:19:23 +0900

    ADD :: PlanTodoService combine

--- src/main/kotlin/com/example/leaveleave/domain/plan/service/PlanService.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/service/PlanService.kt
@@ -5,12 +5,14 @@ import com.example.leaveleave.domain.plan.domain.PlanTodoList
 import com.example.leaveleave.domain.plan.domain.repository.PlanRepository
 import com.example.leaveleave.domain.plan.domain.repository.PlanTodoRepository
 import com.example.leaveleave.domain.plan.exception.PlanNotFoundException
+import com.example.leaveleave.domain.plan.exception.PlanTodoListNotFoundException
 import com.example.leaveleave.domain.plan.presentation.dto.request.PlanRequest
-import com.example.leaveleave.domain.plan.presentation.dto.request.PlanTodoRequest
+import com.example.leaveleave.domain.plan.presentation.dto.response.PlanTodoListResponse
 import com.example.leaveleave.domain.user.facade.UserFacade
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
+import java.util.NoSuchElementException
+import javax.transaction.Transactional
 
 @Service
 class PlanService(
@@ -22,7 +24,6 @@ class PlanService(
         val user = userFacade.getCurrentUser()
 
         val newPlan = Plan(
-            id =  planRequest.planId,
             user = user,
             title = planRequest.title,
             startDate = planRequest.startDate,
@@ -36,16 +37,33 @@ class PlanService(
         }
     }
 
-    fun getPlanById(planId: Long): Plan = planRepository.findById(planId).orElseThrow {
-        PlanNotFoundException("계획을 찾을 수 없음 $planId")
+    fun getPlanById(planId: Long): Plan {
+        return planRepository.findById(planId).orElseThrow {
+            NoSuchElementException("Plan with ID $planId not found")
+        }
+    }
+
+    fun getTodoPlanId(planId: Long): PlanTodoList {
+        return planTodoRepository.getAllByPlanId(planId).firstOrNull()
+            ?: throw PlanTodoListNotFoundException("계획을 찾을 수 없음")
     }
 
     fun deletePlan(planId: Long) {
-        try {
-            planRepository.deleteById(planId)
-        } catch (e: EmptyResultDataAccessException) {
+        val exists = planRepository.existsById(planId)
+        if (!exists) {
             throw PlanNotFoundException("계획을 찾을 수 없음 $planId")
         }
+        planRepository.deleteById(planId)
+    }
+
+    fun addTodo(planId: Long, detailContent: String, todoId: Long): PlanTodoListResponse {
+        val plan = planRepository.findById(planId).orElseThrow {
+            NoSuchElementException("계획 아이디가 존재하지 않음 $planId")
+        }
+        val todo = PlanTodoList(detailContent = detailContent, plan = plan)
+
+        val savedTodo = planTodoRepository.save(todo)
+        return PlanTodoListResponse.fromEntity(savedTodo)
     }
 
     fun deleteTodo(todoId: Long) {
