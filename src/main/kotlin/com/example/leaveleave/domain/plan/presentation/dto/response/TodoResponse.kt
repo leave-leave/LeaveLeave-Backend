1624665bf47f45e656ef603bd94e49e7aa5eb098
eeuunn <haspring@dsm.hs.kr>
2024-02-14 22:36:26 +0900

    ADD :: TodoResponse

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/TodoResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/TodoResponse.kt
@@ -1,16 +1,15 @@
 package com.example.leaveleave.domain.plan.presentation.dto.response
 
-import com.example.leaveleave.domain.plan.domain.Plan
 import com.example.leaveleave.domain.plan.domain.PlanTodoList
 
-class PlanTodoListResponse(
+class TodoResponse(
     val detailContent: String,
     val planId: Long?,
     val todoId : Long?
 ) {
     companion object {
-        fun fromEntity(planTodoList: PlanTodoList): PlanTodoListResponse {
-            return PlanTodoListResponse(
+        fun fromEntity(planTodoList: PlanTodoList): TodoResponse {
+            return TodoResponse(
                 detailContent = planTodoList.detailContent,
                 planId = planTodoList.plan.id,
                 todoId = planTodoList.id
