36fa5823c0f3a834933dbb68b4f1811f27651964
eeuunn <haspring@dsm.hs.kr>
2024-02-14 22:36:35 +0900

    CHORE

--- src/main/kotlin/com/example/leaveleave/domain/plan/domain/PlanTodoList.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/domain/PlanTodoList.kt
@@ -6,7 +6,7 @@ import javax.persistence.*
 class PlanTodoList(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long?,
+    val id: Long,
 
     @Column(columnDefinition = "VARCHAR(40)", nullable = false)
     var detailContent: String,
