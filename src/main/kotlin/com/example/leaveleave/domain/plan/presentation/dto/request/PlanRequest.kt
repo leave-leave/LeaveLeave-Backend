8f38286223f7a28ed4693d46f2ac233320265e8a
eeuunn <haspring@dsm.hs.kr>
2024-02-14 22:37:04 +0900

    UPDATE :: id 추가

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanRequest.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/request/PlanRequest.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.ZonedDateTime
 
 data class PlanRequest(
+    val id : Long,
     val userId: String,
     val startDate: ZonedDateTime,
     val endDate: ZonedDateTime,
