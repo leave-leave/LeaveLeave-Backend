e53f94c93b38673757d213b82ecbafb3c25c9ebd
eeuunn <haspring@dsm.hs.kr>
2024-02-14 10:51:30 +0900

    UPDATE :: Response

--- src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanTodoListResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/plan/presentation/dto/response/PlanTodoListResponse.kt
@@ -5,13 +5,15 @@ import com.example.leaveleave.domain.plan.domain.PlanTodoList
 
 class PlanTodoListResponse(
     val detailContent: String,
-    val plan: Plan
+    val planId: Long?,
+    val todoId : Long?
 ) {
     companion object {
         fun fromEntity(planTodoList: PlanTodoList): PlanTodoListResponse {
             return PlanTodoListResponse(
                 detailContent = planTodoList.detailContent,
-                plan = planTodoList.plan
+                planId = planTodoList.plan.id,
+                todoId = planTodoList.id
             )
         }
     }
