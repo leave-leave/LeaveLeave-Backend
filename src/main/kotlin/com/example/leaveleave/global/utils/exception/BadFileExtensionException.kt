d7640b278d85f535a20755e9d16bdd00cd623ba9
eeuunn <haspring@dsm.hs.kr>
2024-02-16 09:55:38 +0900

    ADD :: BadFileExtensionException

---
+++ src/main/kotlin/com/example/leaveleave/global/utils/exception/BadFileExtensionException.kt
@@ -0,0 +1,6 @@
+package com.example.leaveleave.global.utils.exception
+
+import com.example.leaveleave.global.error.exception.ErrorCode
+import com.example.leaveleave.global.error.exception.LeaveLeaveException
+
+object BadFileExtensionException :  LeaveLeaveException(ErrorCode.BAD_FILE_EXTENSION)
