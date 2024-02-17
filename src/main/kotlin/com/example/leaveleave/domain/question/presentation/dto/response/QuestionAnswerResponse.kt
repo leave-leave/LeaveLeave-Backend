08c89119d45078f6fb96c9a769709a81b022723e
eeuunn <haspring@dsm.hs.kr>
2024-02-17 11:30:25 +0900

    ADD :: QuestionAnswer Response

---
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/dto/response/QuestionAnswerResponse.kt
@@ -0,0 +1,7 @@
+package com.example.leaveleave.domain.question.presentation.dto.response
+
+class QuestionAnswerResponse(
+    val accountId: Long,
+    val questionId: Long,
+    val answer: Boolean,
+)
