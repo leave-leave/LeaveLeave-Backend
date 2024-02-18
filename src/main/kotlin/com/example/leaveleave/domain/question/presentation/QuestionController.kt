a774a280025ed98fe00d6ce356d4245559ccc622
eeuunn <haspring@dsm.hs.kr>
2024-02-18 13:44:11 +0900

    UPDATE :: add questionAnswer response sheet registration and inquiry

--- src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
@@ -1,17 +1,36 @@
 package com.example.leaveleave.domain.question.presentation
 
 import com.example.leaveleave.domain.question.domain.Question
+import com.example.leaveleave.domain.question.domain.QuestionAnswer
+import com.example.leaveleave.domain.question.presentation.dto.request.QuestionAnswerRequest
 import com.example.leaveleave.domain.question.service.QuestionService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/question")
 class QuestionController(
     private val questionService: QuestionService
 ) {
-    @GetMapping("/question")
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
     fun getQuestion(): List<Question> {
         return questionService.getQuestion()
     }
 
+    @PostMapping("/{account-id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun postQuestionAnswer(questionAnswerRequest: QuestionAnswerRequest){
+        return questionService.postQuestionAnswer(questionAnswerRequest)
+    }
+
+    @GetMapping("/{account-id}")
+    @ResponseStatus(HttpStatus.OK)
+    fun getQuestionAnswer(): MutableList<QuestionAnswer>{
+        return questionService.getQuestionAnswer()
+    }
 }
--- src/main/kotlin/com/example/leaveleave/domain/question/service/QuestionService.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/service/QuestionService.kt
@@ -1,14 +1,33 @@
 package com.example.leaveleave.domain.question.service
 
 import com.example.leaveleave.domain.question.domain.Question
+import com.example.leaveleave.domain.question.domain.QuestionAnswer
+import com.example.leaveleave.domain.question.domain.repository.QuestionAnswerRepository
 import com.example.leaveleave.domain.question.domain.repository.QuestionRepository
+import com.example.leaveleave.domain.question.presentation.dto.request.QuestionAnswerRequest
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class QuestionService(
-    private val questionRepository: QuestionRepository
+    private val questionRepository: QuestionRepository,
+    private val questionAnswerRepository: QuestionAnswerRepository,
 ) {
     fun getQuestion(): List<Question> {
         return questionRepository.findAll()
+    }
+
+    fun postQuestionAnswer(questionAnswerRequest: QuestionAnswerRequest){
+        val questionAnswer = QuestionAnswer(
+            questionAnswerRequest.id,
+            questionAnswerRequest.questionAnswer,
+            questionAnswerRequest.accountId,
+            questionAnswerRequest.question
+        )
+        questionAnswerRepository.save(questionAnswer)
+    }
+
+    fun getQuestionAnswer(): MutableList<QuestionAnswer> {
+        return questionAnswerRepository.findAll()
     }
 }
