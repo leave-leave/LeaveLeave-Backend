eedf1a4bbf981b29fdb6be6b67caeaa9bbb39d2f
eeuunn <haspring@dsm.hs.kr>
2024-02-18 13:42:01 +0900

    ADD :: QuestionAnswer entity

--- src/main/kotlin/com/example/leaveleave/domain/question/domain/QuestionAnswer.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/domain/QuestionAnswer.kt
@@ -1,6 +1,7 @@
 package com.example.leaveleave.domain.question.domain
 
-import com.example.leaveleave.domain.like.domain.Like
+import com.example.leaveleave.domain.plan.domain.Plan
+import com.example.leaveleave.domain.user.domain.User
 import javax.persistence.*
 
 @Entity(name = "tbl_question_answer")
@@ -12,9 +13,18 @@ class QuestionAnswer(
     @Column
     val questionAnswer: Boolean,
 
-    @OneToMany(mappedBy = "question", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val questionId: MutableList<Like> = mutableListOf(),
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id")
+    var accountId: User,
 
-    @OneToMany(mappedBy = "question", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val accountId: MutableList<Like> = mutableListOf(),
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "question_id")
+    var question: Question
+) {
+    constructor(questionAnswer: Boolean, accountId: User, question: Question) : this(
+        0,
+        questionAnswer = true,
+        accountId,
+        question
     )
+}
