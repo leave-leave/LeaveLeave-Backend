9fa1d43785902b34d4420482b171adc6e7fd1c30
eeuunn <haspring@dsm.hs.kr>
2024-02-18 14:43:53 +0900

    DELETE

--- src/main/kotlin/com/example/leaveleave/global/utils/exception/BadFileExtensionException.kt
+++
@@ -1,6 +0,0 @@
-package com.example.leaveleave.global.utils.exception
-
-import com.example.leaveleave.global.error.exception.ErrorCode
-import com.example.leaveleave.global.error.exception.LeaveLeaveException
-
-object BadFileExtensionException : LeaveLeaveException(ErrorCode.BAD_FILE_EXTENSION)
