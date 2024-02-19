c05b748c38d524f6053a52372eeeaf2b95f1ad04
eeuunn <haspring@dsm.hs.kr>
2024-02-19 21:21:19 +0900

    UPDATE :: QuestionService

--- src/main/kotlin/com/example/leaveleave/domain/question/service/QuestionService.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/service/QuestionService.kt
@@ -4,27 +4,39 @@ import com.example.leaveleave.domain.question.domain.Question
 import com.example.leaveleave.domain.question.domain.QuestionAnswer
 import com.example.leaveleave.domain.question.domain.repository.QuestionAnswerRepository
 import com.example.leaveleave.domain.question.domain.repository.QuestionRepository
+import com.example.leaveleave.domain.question.exception.QuestionAnswerException
 import com.example.leaveleave.domain.question.presentation.dto.request.QuestionAnswerRequest
+import com.example.leaveleave.domain.question.presentation.dto.response.QuestionAnswerResponse
+import com.example.leaveleave.domain.user.domain.repository.UserRepository
+import com.example.leaveleave.domain.user.facade.UserFacade
 import org.springframework.stereotype.Service
+import java.util.NoSuchElementException
 import javax.transaction.Transactional
 
 @Service
 class QuestionService(
     private val questionRepository: QuestionRepository,
     private val questionAnswerRepository: QuestionAnswerRepository,
+    private val userFacade: UserFacade
 ) {
     fun getQuestion(): List<Question> {
         return questionRepository.findAll()
     }
 
-    fun postQuestionAnswer(questionAnswerRequest: QuestionAnswerRequest){
-        val questionAnswer = QuestionAnswer(
-            questionAnswerRequest.user,
-            questionAnswerRequest.questionAnswer,
-            questionAnswerRequest.accountId,
-            questionAnswerRequest.question
-        )
-        questionAnswerRepository.save(questionAnswer)
+    @Transactional
+    fun postQuestionAnswer(questionAnswerRequest: QuestionAnswerRequest) {
+        val user = userFacade.getCurrentUser()
+        questionAnswerRequest.answers?.map { answer ->
+            val question = questionRepository.getQuestionById(answer.questionId)
+            questionAnswerRepository.save(
+                QuestionAnswer(
+                    id = null,
+                    user = user,
+                    questionAnswer = answer.questionAnswer,
+                    question = question
+                )
+            )
+        }
     }
 
     fun getQuestionAnswer(): MutableList<QuestionAnswer> {
