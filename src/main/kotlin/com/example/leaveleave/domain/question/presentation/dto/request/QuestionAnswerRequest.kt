64a581fc429e3a94fa1ddb6eaee054ba7a785b0e
eeuunn <haspring@dsm.hs.kr>
2024-02-19 23:36:11 +0900

    UPDATE :: QuestionAnswerRequest

--- src/main/kotlin/com/example/leaveleave/domain/question/presentation/dto/request/QuestionAnswerRequest.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/dto/request/QuestionAnswerRequest.kt
@@ -1,11 +1,14 @@
 package com.example.leaveleave.domain.question.presentation.dto.request
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 
 data class QuestionAnswerPair(
     val questionId: Long,
+    @JsonProperty("questionAnswer")
     val questionAnswer: Boolean,
 )
 
 data class QuestionAnswerRequest(
-    val answers: List<QuestionAnswerPair>?,
+    val answers: List<QuestionAnswerPair>? = emptyList()
 )
