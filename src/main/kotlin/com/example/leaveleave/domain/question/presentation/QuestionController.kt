e75c95342346abac064155d7a2c074b58a3166a5
eeuunn <haspring@dsm.hs.kr>
2024-02-19 23:36:24 +0900

    UPDATE :: QuestionController

--- src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/QuestionController.kt
@@ -7,6 +7,7 @@ import com.example.leaveleave.domain.question.service.QuestionService
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
@@ -24,7 +25,7 @@ class QuestionController(
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    fun postQuestionAnswer(questionAnswerRequest: QuestionAnswerRequest){
+    fun postQuestionAnswer(@RequestBody questionAnswerRequest: QuestionAnswerRequest){
         return questionService.postQuestionAnswer(questionAnswerRequest)
     }
 
