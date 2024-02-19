20de238fc43cc48ab222c835b6f0df9acc3e675e
eeuunn <haspring@dsm.hs.kr>
2024-02-19 21:19:39 +0900

    UPDATE :: Question domain Repository

--- src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionAnswerRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionAnswerRepository.kt
@@ -1,8 +1,9 @@
 package com.example.leaveleave.domain.question.domain.repository
 
+import com.example.leaveleave.domain.question.domain.Question
 import com.example.leaveleave.domain.question.domain.QuestionAnswer
+import com.example.leaveleave.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface QuestionAnswerRepository : JpaRepository<QuestionAnswer,Long>{
-
+interface QuestionAnswerRepository : JpaRepository<QuestionAnswer, Long> {
 }
--- src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionRepository.kt
@@ -4,5 +4,5 @@ import com.example.leaveleave.domain.question.domain.Question
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionRepository : JpaRepository<Question, Long> {
-
+    fun getQuestionById(questionId:Long): Question
 }
