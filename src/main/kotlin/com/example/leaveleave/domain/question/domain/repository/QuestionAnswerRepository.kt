10082ae43a84f4bbc5526f72f03a1d2e54215a86
eeuunn <haspring@dsm.hs.kr>
2024-02-21 05:45:32 +0900

    UPDATE :: QuestionAnswerRepository

--- src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionAnswerRepository.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/domain/repository/QuestionAnswerRepository.kt
@@ -6,4 +6,5 @@ import com.example.leaveleave.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionAnswerRepository : JpaRepository<QuestionAnswer, Long> {
+    fun findByUser(accountId: User): Long
 }
