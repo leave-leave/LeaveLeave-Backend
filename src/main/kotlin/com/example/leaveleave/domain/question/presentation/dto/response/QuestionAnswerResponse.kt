a9ea9d69e81d5fa4908d6d53a401a700f1e60be1
eeuunn <haspring@dsm.hs.kr>
2024-02-21 14:12:47 +0900

    UPDATE :: QuestionAnswerResponse

--- src/main/kotlin/com/example/leaveleave/domain/question/presentation/dto/response/QuestionAnswerResponse.kt
+++ src/main/kotlin/com/example/leaveleave/domain/question/presentation/dto/response/QuestionAnswerResponse.kt
@@ -2,10 +2,20 @@ package com.example.leaveleave.domain.question.presentation.dto.response
 
 import com.example.leaveleave.domain.question.domain.QuestionAnswer
 import com.example.leaveleave.domain.question.presentation.dto.request.QuestionAnswerPair
+import com.example.leaveleave.domain.question.presentation.dto.request.QuestionAnswerRequest
 import com.example.leaveleave.domain.user.domain.User
 
 data class QuestionAnswerResponse(
     val accountId: User,
-    val answers: List<QuestionAnswerPair>
-)
+    val answers: List<QuestionAnswerPair>?
+){
+    companion object{
+        fun format(questionAnswerRequest: List<QuestionAnswerPair>?, accountId: User): QuestionAnswerResponse{
+            return QuestionAnswerResponse(
+                accountId = accountId,
+                answers = questionAnswerRequest,
+            )
+        }
+    }
+}
 
